34ec42df8345e993b450e033cab050faa7dab632
Marc Ginesta <mginesta@magento.com>
2017-05-12 13:25:26 +0200

    Managing React Component state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            txt: 'this is the state string',
+            cat: 0
+        }
+    }
+    update( e ){
+        this.setState({txt: e.target.value})
+    }
     render(){
-        let propsText = this.props.text;
         return(
             <div>
-                <h1>{propsText}</h1>
-                <b>{this.props.cat}</b>
+                <input type="text"
+                       onChange={this.update.bind(this)} />
+                <h1>{this.state.txt} - {this.state.cat}</h1>
             </div>
         )
     }
-}
-
-App.propTypes = {
-    text: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-    text: "this is the default text",
-    cat: 4
 }
 
 export default App;
