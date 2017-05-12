cfb57d233c41332986fb0b28f531a680552466cf
Marc Ginesta <mginesta@magento.com>
2017-05-12 13:15:10 +0200

    How to structure JSX to render content correctly

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class App extends React.Component {
     render(){
-        return React.createElement('h1', null, 'Hello Folks')
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <b>Bold</b>
+            </div>
+        )
     }
 }
 
