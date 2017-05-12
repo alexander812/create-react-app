f7e618b2caba06ee211b6e4e63c8f18388c3aba9
Marc Ginesta <mginesta@magento.com>
2017-05-12 13:32:04 +0200

    Accessing Nested Data with Reacts props.children

--- src/App.js
+++ src/App.js
@@ -5,25 +5,45 @@ class App extends React.Component {
         super();
         this.state = {
             txt: 'this is the state string',
-            cat: 0
+            cat: 0,
+            name: ''
         }
+
+        this.updateProperties = this.updateProperties.bind(this);
     }
-    update( e ){
-        this.setState({txt: e.target.value})
+
+    updateProperties(e){
+        var name = e.target.name;
+        var value = e.target.value;
+        this.setState({
+            [name]: value,
+            name: name
+        })
     }
+
     render(){
         return(
             <div>
-                <Widget update={this.update.bind(this)} />
-                <Widget update={this.update.bind(this)} />
-                <Widget update={this.update.bind(this)} />
+                <Input id="txt" update={this.updateProperties} />
+                <Input id="cat" update={this.updateProperties} />
                 <h1>{this.state.txt} - {this.state.cat}</h1>
+                <h1>Last updated {this.state.name}</h1>
+                <Button>I <Heart /> React</Button>
             </div>
         )
     }
 }
 
-const Widget = (props) =>
-    <input type="text" onChange={props.update} />
+const Input = (props) =>
+    <input id={props.id} name={props.id} type="text" onChange={props.update} />
+
+
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component {
+    render(){
+        return <span>&hearts;</span>
+    }
+}
 
 export default App;
