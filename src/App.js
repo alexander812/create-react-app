57b01a29bafbba9a844970bc3adb62cd45abd752
Marc Ginesta <mginesta@magento.com>
2017-05-12 13:10:30 +0200

    Checking differences between stateful and stateless

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class App extends React.Component {
     render(){
-        return <h1>Hello World!</h1>
+        return React.createElement('h1', null, 'Hello Folks')
     }
 }
 
